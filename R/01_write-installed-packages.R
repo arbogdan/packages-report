9bf5653dbde8076a766f9da317348611e7f8ddf5
alex.bogdan <abogdan@massmutual.com>
2018-08-14 10:34:24 -0400

    Aggregate and output R library package list

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,11 @@
 ## deja vu from yesterday!
 
 ## create a data frame of your installed packages
+library(tidyverse)
+library(fs)
+
+df <- installed.packages() %>%
+  as_tibble()
 
 ## keep the variables
 ##   * Package
@@ -9,10 +14,15 @@
 ##   * Priority
 ##   * Built
 
+df <- df %>%
+  select(Package, LibPath, Version, Priority, Built)
+
 ## write it to data/installed-packages.csv
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+write.csv(df, 'data/installed-packages.csv')
+
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
